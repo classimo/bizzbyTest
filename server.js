437c2f1dafdbb8b7e8b4f297d65083b1f52d674f
Paulius <paulius@investup.co>
2016-06-16 17:45:20 +0100

    z

--- server.js
+++ server.js
@@ -2,14 +2,8 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
-var port = process.env.PORT || 80;
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(port, 'localhost', function (err) {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:8080');
-});
+}).listen(process.env.PORT || 5000);
